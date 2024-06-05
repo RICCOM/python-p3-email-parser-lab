cf2884446b43403b203f192f3ac1fe1735660b87
RICCOM <ericomun@gmail.comgit>
2024-06-05 15:25:48 +0300

    using regex to search and validate strings matching specific patterns

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,17 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        
+        email_pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
+        
+        addresses = re.split(r'[,\s]+', self.email_addresses)
+        
+        valid_addresses = [address for address in addresses if email_pattern.fullmatch(address)]
+        
+        unique_addresses = sorted(set(valid_addresses))
+        return unique_addresses
